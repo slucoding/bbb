e4cb53a63736aca6b323e0acf0e325b67417bbce
slucoding <slucoding@gmail.com>
2020-06-21 07:28:06 +0600

    ADDING RSS FOR POSTS. 1.Creation feeds: - created the file 'blog/feeds' and paste the code. 2.Creation URL. 3.Editing the blog/urls.py file: - import completed; - url template created. 4.Creation link for RSS: - created link & string of link;

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,6 +8,6 @@ sitemaps = {'posts': PostSitemap}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
-
